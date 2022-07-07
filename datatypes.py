cc71b6685abb1031cba7645b2f459919be640fe3
Srinandhan20 <107259556+Srinandhan20@users.noreply.github.com>
2022-07-07 10:35:37 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+# a=1 ; b='python'
+# print(type(a))
+# print(type(b))
+# print(str(a)+b)
+# print(b+str(a))
+# x='Samsung'; y='M'; z=31
+# print(x+y+str(z))
+# print(x+'-'+y+' '+str(z))
+
+# write a program to print hello-world@123
+a='hello'
+b='world'
+c=123
+print(a+'-'+b+'@'+str(c))
